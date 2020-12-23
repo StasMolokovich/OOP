e758871f1b3d0b87036c0eaff85124e97a9b9e80
StasMolokovich <43597568+StasMolokovich@users.noreply.github.com>
2020-12-23 10:49:16 +0300

    Add files via upload

---
+++ CS/lab7/lab_7.2/lab_7.2/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace lab_7._2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[] { 
+            new Point(0, this.Height/2),
+            new Point(this.Width/2, this.Height),
+            new Point(this.Width, this.Height/2),
+            new Point(this.Width/2, 0)});
+            Region myRegion = new Region(myPath); this.Region = myRegion;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
